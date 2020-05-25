f41bba0b78f347a5978a0c4660cae38ded7e8946
Ben Haus <ben@little-haus.com>
2020-05-25 11:54:06 -0400

    server set up and recieving 'api: up' from localhost:5005/

---
+++ api/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+// const authenticate = require('../api/auth/authenticationMiddleware');
+// const authRouter = require('../api/auth/authRouter');
+// const usersRouter = require('../api/users/usersRouter');
+// const trucksRouter = require('../api/trucks/trucksRouter');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// server.use('/api', authRouter);
+// server.use('/api/jokes', authenticate, jokesRouter);
+// server.use("/api/users/", usersRouter);
+// server.use('/api/trucks/', trucksRouter);
+
+
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "up" });
+});
+
+module.exports = server;
